2ed6399e2a915b96b7c5b9958bc83d0422fe99e8
Hang Nguyen <hang.panbee@gmail.com>
2021-08-02 16:21:52 -0400

    Time: 52 ms (81.30%), Space: 17.7 MB (23.54%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head: return False
+        slow, fast = head, head.next
+        
+        # What is the difference between is and ==?
+        # is point to an object
+        # == compares value
+        while slow != fast:
+            if fast == None or fast.next == None: return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
+        
