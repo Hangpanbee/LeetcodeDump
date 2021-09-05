40b4fff4dfa1904948cf73b2f216b5d3a0602890
Hang Nguyen <hang.panbee@gmail.com>
2021-09-05 12:08:42 -0400

    Time: 72 ms (44.00%), Space: 15.6 MB (38.00%) - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        sumWaterTrapped = 0
+        mono_stack = []
+
+
+        for bar in range(len(height)):
+            
+            while mono_stack and height[bar] > height[mono_stack[-1]]:
+                poppedBarIndex = mono_stack.pop()
+                poppedBar = height[poppedBarIndex]
+                if not len(mono_stack): break
+                #if there is only one ele that is smaller than height bar, which means there is no puddle formed
+                tall = min(height[bar], height[mono_stack[-1]]) - poppedBar
+                length = bar - mono_stack[-1] - 1
+                
+                sumWaterTrapped += tall*length
+
+            mono_stack.append(bar)
+            
+        return sumWaterTrapped
