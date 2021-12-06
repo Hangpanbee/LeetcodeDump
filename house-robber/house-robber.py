1c66a96fb1b21177d7fbefbf0623304496faf6d3
Hang Nguyen <hang.panbee@gmail.com>
2021-12-06 12:37:33 -0500

    Time: 32 ms (66.08%), Space: 14.1 MB (93.52%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -4,10 +4,10 @@ class Solution:
         dp = [0] * len(nums)
         
         for i in range(len(nums)):
-            if i < 2:
+            if i < 1:
                 dp[i] = nums[i]
             else:
-                dp[i] = max(dp[i-2], dp[i-3]) + nums[i]
+                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
       
         return max(dp[-1], dp[-2])
         
