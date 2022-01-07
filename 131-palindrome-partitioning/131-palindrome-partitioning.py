9717615d899d0a7822e91eb725df3f86721c4427
Hang Nguyen <hang.panbee@gmail.com>
2022-01-07 09:55:34 -0500

    Time: 889 ms (39.85%), Space: 30.9 MB (21.77%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        self.ans = []
+        def backtrack(s, curr_pali):
+
+            if len(s) == 0:
+                self.ans.append(curr_pali)
+            
+            for i in range(1, len(s)+1):
+                curr_chopped, next_s = s[:i], s[i:len(s)]
+                if self.isPali(curr_chopped):
+                    backtrack(next_s, curr_pali+[curr_chopped])
+            return None
+        
+        backtrack(s, [])
+        #print(self.ans)   
+        return self.ans
+        
+        
+        
+    def isPali(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]: return False
+            else: 
+                l += 1
+                r -= 1
+        return True
+                
+        
