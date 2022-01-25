d4e7baea2a8c26164db23b7276b18b3f4c8e48b7
Hang Nguyen <hang.panbee@gmail.com>
2022-01-25 11:50:25 -0500

    Time: 1476 ms (8.35%), Space: 17.5 MB (78.18%) - LeetHub

---
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSwaps(self, data_list: List[int]) -> int:
+        window_size = sum(data_list)
+
+        l, r = 0, 0
+        window_sum = 0
+        ans = 99999999
+        while r < len(data_list):
+
+            window_sum += data_list[r]
+            if (r - l + 1) == window_size:
+
+                ans = min(ans, window_size - window_sum)
+
+                window_sum -= data_list[l]
+
+                l += 1
+
+            r += 1
+
+    
+        return ans if ans != 99999999 else 0
+                
+        
