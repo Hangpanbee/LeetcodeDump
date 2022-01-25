f77947dd6d18a2c4626f860aa55c872aa48e3043
Hang Nguyen <hang.panbee@gmail.com>
2022-01-25 11:51:18 -0500

    Time: 832 ms (68.39%), Space: 17.5 MB (78.18%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -4,19 +4,19 @@ class Solution:
 
         l, r = 0, 0
         window_sum = 0
-        ans = 99999999
+        ans = 0
         while r < len(data_list):
 
             window_sum += data_list[r]
             if (r - l + 1) == window_size:
 
-                ans = min(ans, window_size - window_sum)
+                ans = max(ans, window_sum)
 
                 window_sum -= data_list[l]
 
                 l += 1
             r += 1
 
-        return ans if ans != 99999999 else 0
+        return window_size - ans
                 
         
