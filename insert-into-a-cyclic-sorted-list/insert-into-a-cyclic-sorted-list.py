baaaa2dcf6273e34ebee3c0b9d3208f4ba8de072
Hang Nguyen <hang.panbee@gmail.com>
2022-02-04 15:35:23 -0500

    Time: 67 ms, Memory: 14.8 MB - LeetHub

---
+++ insert-into-a-cyclic-sorted-list/insert-into-a-cyclic-sorted-list.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        
+        newNode = Node(insertVal, None)
+        if not head: 
+            newNode.next = newNode
+            return newNode
+        dummy = head
+        while head:
+            pivot_start_pt = insertVal < head.next.val < head.val
+            end_pivot_pt = head.next.val < head.val < insertVal
+            not_a_pivot = head.val <= insertVal <= head.next.val
+
+            if (pivot_start_pt or end_pivot_pt or not_a_pivot) or head.next == dummy:
+                print("start", pivot_start_pt)
+                print("end", end_pivot_pt)
+                print("not", not_a_pivot)
+                tmp = head.next
+                head.next = newNode
+                newNode.next = tmp
+                break    
+            head = head.next
+
+
+        return dummy
