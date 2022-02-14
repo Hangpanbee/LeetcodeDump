7a96a8cb0ca316c888fbe4142549621825e065bc
Hang Nguyen <hang.panbee@gmail.com>
2022-02-14 16:14:32 -0500

    Time: 1654 ms (17.34%), Space: 32.2 MB (33.49%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.py
@@ -1,24 +1,17 @@
 class Solution:
-    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        heap = []
-        
-        n1, n2 = 0, 0
-        heapq.heappush(heap, [nums1[n1]+nums2[n2], n1, n2])
-        ans = []
-        tracker = {}
-        while len(ans) < k and heap:
-            curr_choice, n1, n2 = heapq.heappop(heap)
-            ans.append([nums1[n1], nums2[n2]])
-           
-            if n1+1 < len(nums1) and (n1+1, n2) not in tracker:
-                left_choice = nums1[n1+1] + nums2[n2]
-                heapq.heappush(heap, [left_choice, n1+1, n2])
-                tracker[(n1+1, n2)] = True
-            if n2+1 < len(nums2) and (n1, n2+1) not in tracker:
-                down_choice = nums2[n2+1] + nums1[n1]
-                heapq.heappush(heap, [down_choice, n1, n2+1])
-                tracker[(n1, n2+1)] = True
-                
 
-        return ans
+    def kSmallestPairs(self, nums1, nums2, k):
+        queue = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+        push(0, 0)
+        pairs = []
+        while queue and len(pairs) < k:
+            _, i, j = heapq.heappop(queue)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        return pairs
         
