1e6ab1dbd9342783e9f7b1855dc0138108ab9a19
Hang Nguyen <hang.panbee@gmail.com>
2022-02-25 12:15:53 -0500

    Time: 30 ms (92.63%), Space: 13.9 MB (75.81%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
@@ -2,21 +2,15 @@ class Solution:
     def twoEggDrop(self, n: int) -> int:
         if n == 1: return 1
         
-        def isPossible(n_max):
-            running_sum = 0
-            while running_sum < n:
-                if n_max == 0: return False
-                running_sum += n_max
-                n_max -= 1
- 
-            return True
         
         i_min, i_max = 2, (n+1)//2
         while i_min < i_max:
             mid = i_min + (i_max - i_min)//2
-            if isPossible(mid):
+            isPossible = ((mid+1)*(mid))/2
+ 
+            if isPossible >= n:
                 i_max = mid
-            elif not isPossible(mid):
+            elif isPossible < n:
                 i_min = mid + 1
                 
             
