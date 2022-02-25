79520621159bf3093ab629adf063001223e64892
Hang Nguyen <hang.panbee@gmail.com>
2022-02-25 09:36:48 -0500

    Time: 38 ms (62.44%), Space: 13.9 MB (68.52%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -4,7 +4,6 @@ class Solution:
         version2 = list(map(lambda x: int(x), version2.split(".")))
         if len(version1) > len(version2): version2 += [0]*(len(version1)-len(version2))
         if len(version1) < len(version2): version1 += [0]*(len(version2)-len(version1))
-        print(version1, version2)
         for v1, v2 in zip(version1, version2):
             if v1 > v2: return 1
             if v1 < v2: return -1
