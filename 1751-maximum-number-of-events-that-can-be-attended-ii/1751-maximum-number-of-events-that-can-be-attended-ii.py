1373574d8a83e4b2ac157e3bc0b3ce5d2bfcbd9f
Hang Nguyen <hang.panbee@gmail.com>
2022-03-01 14:26:03 -0500

    Time: 2227 ms (12.45%), Space: 239.6 MB (5.03%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        ls = sorted(set([i  for d in events for i in {d[0]-1, d[1]}]))
+        #print(ls)
+        events =[[d[0]-1, d[1], d[2]]  for d in events]
+        events.sort()
+ 
+        memoize = {}
+        curr_event = 0
+        @lru_cache(None)
+        def dp(k, limit, curr_event):
+            if k == 0: return 0
+            if curr_event >= len(events): return 0
+            
+            start, end, value = events[curr_event]
+            take, notake = 0, 0
+            if limit <= start:
+                take = value + dp(k-1, end, curr_event+1)
+            
+            notake = dp(k, limit, curr_event+1)
+            
+            memoize[(k, limit, curr_event)] = max(take, notake)
+           
+            return memoize[(k, limit, curr_event)]
+  
+        return dp(k, -1, 0)
+            
+    
+#    [[1,2,4],[3,4,7],[1,4,9],[5,6,3]]
+#2
