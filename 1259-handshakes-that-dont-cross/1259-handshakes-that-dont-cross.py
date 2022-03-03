b649cb838866558c0bad047eebd2aeb5d80ebc0e
Hang Nguyen <hang.panbee@gmail.com>
2022-03-03 14:36:14 -0500

    Time: 423 ms (78.39%), Space: 13.8 MB (88.27%) - LeetHub

---
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfWays(self, numPeople: int) -> int:
+        dp = [1,1,2,5] + [0] * (numPeople//2-3)
+        if (numPeople // 2) <= 3: return dp[numPeople//2]
+        pair = numPeople//2 
+        running_sum = 8
+        for i in range(4, pair + 1):
+            r = i
+            for j in range((i+1)//2):
+                if (r-1) != j:
+                    dp[i] += dp[r-1]*dp[j]*2
+                else:
+                    dp[i] += dp[r-1]*dp[j]
+    
+                r -= 1
+        
+        
+        return dp[-1] % (10**9+7)
