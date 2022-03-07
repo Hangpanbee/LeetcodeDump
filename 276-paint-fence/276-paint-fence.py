e08bdffb70e9be1689305c254b477e737661e96f
Hang Nguyen <hang.panbee@gmail.com>
2022-03-07 14:01:02 -0500

    Time: 24 ms (98.04%), Space: 13.9 MB (86.10%) - LeetHub

---
+++ 276-paint-fence/276-paint-fence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n < 3: return k**n
+  
+        same_color, diff_color = 0, k
+        for i in range(1, n):
+            curr_diff_color = (same_color + diff_color)*(k-1)
+            curr_same_color = (diff_color)
+            same_color = curr_same_color
+            diff_color = curr_diff_color
+            #print(same_color, diff_color)
+            
+            
+        return same_color + diff_color
+        
+        
