fb5a6f720a8dbe6f6582b68ad5c5333c575e86c4
Hang Nguyen <hang.panbee@gmail.com>
2022-03-16 16:34:00 -0400

    Time: 3016 ms (46.36%), Space: 22 MB (33.37%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.py
@@ -0,0 +1,47 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        
+            
+        self.ans = 0  
+            
+        def mergeSort(nums):
+            if len(nums) == 1:
+                return nums
+            
+            left = mergeSort(nums[:len(nums)//2])
+            right = mergeSort(nums[len(nums)//2:])
+
+            return merge(left, right)
+            
+            
+        def merge(left, right):
+            out = []
+            l, r = 0, 0
+
+            while r < len(right):
+
+                self.ans += len(left) - bisect.bisect_right(left, right[r]*2, l, len(left))
+                r += 1
+            
+            r = 0
+            while l < len(left) and r < len(right):
+                if left[l] >= right[r]:
+                    out.append(right[r])
+                    r+=1
+                elif left[l] < right[r]:
+                    out.append(left[l])
+                    l+=1
+ 
+            #print(self.ans)
+            if l < len(left):
+                out += left[l:]
+                #out.extend(left[l:])
+            if r < len(right):
+                out += right[r:]
+                #out.extend(right[r:])
+         
+            return out
+        
+        mergeSort(nums)
+        return self.ans
+                
