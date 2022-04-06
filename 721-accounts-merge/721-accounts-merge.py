08c01865b8c278b585b19cd22ea9068c6c1a6c8b
Hang Nguyen <hang.panbee@gmail.com>
2022-04-06 15:38:04 -0400

    Time: 567 ms (11.00%), Space: 17.7 MB (91.35%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -12,17 +12,18 @@ class DisjointSet:
     def union(self, acc1, acc2):
         acc1Parent = self.find(acc1)
         acc2Parent = self.find(acc2)
-        if acc1Parent == acc2Parent: return
+        if acc1Parent == acc2Parent: return acc1Parent
         
         acc1Rank = self.rank[acc1Parent]
         acc2Rank = self.rank[acc2Parent]
         if acc1Rank >= acc2Rank:
             self.root[acc2] = self.root[acc1]
             self.rank[acc1Parent] += self.rank[acc2Parent]
+            return self.root[acc2]
         else:
             self.root[acc1] = self.root[acc2]
             self.rank[acc2Parent] += self.rank[acc1Parent]
-        
+            return self.root[acc1]
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
@@ -31,8 +32,8 @@ class Solution:
         for i in range(len(accounts)):
             for j in range(1, len(accounts[i])):
                 if accounts[i][j] in seen:
-                    DS.union(seen[accounts[i][j]], DS.find(i))
-                seen[accounts[i][j]] = i
+                    seen[accounts[i][j]] = DS.union(seen[accounts[i][j]], DS.find(i))
+                else: seen[accounts[i][j]] = i
         
         
         ans = collections.defaultdict(list)
