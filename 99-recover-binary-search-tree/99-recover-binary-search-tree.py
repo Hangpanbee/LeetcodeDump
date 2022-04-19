1c9f2218fc7211622822653f5a1f170c6805be61
Hang Nguyen <hang.panbee@gmail.com>
2022-04-19 13:40:18 -0400

    Time: 97 ms (53.31%), Space: 14.2 MB (65.76%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -10,23 +10,32 @@ class Solution:
         Do not return anything, modify root in-place instead.
         """
         
-        self.problem1, self.problem2 = None, None
+        self.problem1, self.problem2, self.problem3 = None, None, None
+        self.tip = None
         ans = []
         def dfs2(root):
             if not root: return
             
             dfs2(root.left)
+            if self.tip and root.val < self.tip.val:
+                if not self.problem1: self.problem1 = self.tip
+                if not self.problem2: self.problem2 = root
+                self.problem3 = root
+            self.tip = root
             ans.append(root)
             dfs2(root.right)
             
             
         dfs2(root)
-        newans = sorted(ans, key=lambda x: x.val)
- 
-        for i in range(len(ans)):
-            if ans[i] != newans[i]:
-                if not self.problem1: self.problem1 = ans[i]
-                self.problem2 = ans[i]
         
-        self.problem1.val, self.problem2.val = self.problem2.val, self.problem1.val
-        #print(self.problem1, self.problem2)
+        ans = [self.problem1, self.problem2, self.problem3]
+        
+        newAns = [self.problem1.val, self.problem2.val, self.problem3.val]
+        newAns.sort()
+   
+        
+        
+        for i in range(len(ans)):
+            ans[i].val = newAns[i]
+        
+    
