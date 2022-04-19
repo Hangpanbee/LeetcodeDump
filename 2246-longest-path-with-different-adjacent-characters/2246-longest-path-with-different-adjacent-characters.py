a7a0a88aa832186cc075ce22e2b9773bb4705778
Hang Nguyen <hang.panbee@gmail.com>
2022-04-19 14:56:24 -0400

    Time: 2439 ms (44.01%), Space: 161.1 MB (38.73%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -4,13 +4,12 @@ class Solution:
         tree = collections.defaultdict(list)
         for i,v in enumerate(parent):
             tree[v].append(i)
-        #print(tree)
-            
+
         def helper(root):
             if tree[root]:          
                 maxBranchDia = 0
                 maxBranchHeight = 0
-        
+                secondMaxBranchHeight = 0
                 branchHeight = []
             
                 for child in tree[root]:
@@ -21,19 +20,15 @@ class Solution:
                     else:
                         aBranchHeight = 0
                         aBranchDia = 0
-                    maxBranchDia = max(maxBranchDia, aBranchDia)
-                    maxBranchHeight = max(maxBranchHeight, aBranchHeight)
-                    #print('inside', child, aBranchHeight, aBranchDia)
-                    branchHeight.append(aBranchHeight)
-                if (len(branchHeight) < 2):
-                    branchHeight.append(0)
-                branchHeight.sort()
-                if len(branchHeight) >= 2:
-                    
-                    maxBranchDia = max(maxBranchDia,  1 + branchHeight[-1] + branchHeight[-2])
+            
+                    if aBranchHeight > maxBranchHeight:
+                        maxBranchHeight, secondMaxBranchHeight = aBranchHeight, maxBranchHeight
+                    elif aBranchHeight > secondMaxBranchHeight:
+                        secondMaxBranchHeight = aBranchHeight
+  
+                maxBranchDia = max(maxBranchDia,  1 + maxBranchHeight + secondMaxBranchHeight)
                 
-                #print(maxBranchHeight, maxBranchDia, root)
-                #print(branchHeight)
+ 
                 
                 return max(maxBranchDia, maxBranchHeight), maxBranchHeight
             else: return 0, 0
