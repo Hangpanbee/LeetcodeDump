c3608afcc83a84392a2033bfb5ff49a39aed6ff4
Hang Nguyen <hang.panbee@gmail.com>
2022-04-20 14:27:58 -0400

    Time: 177 ms (44.75%), Space: 15.7 MB (7.62%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.py
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -1,24 +1,42 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        heap = []
-        heapq.heappush(heap, (grid[0][0], 0, 0, {(0,0)}))
+        if n == 1: return grid[0][0]
+        startHeap = []
+        endHeap = []
+        sVisited = {(0,0): 0}
+        eVisited = {(n-1, n-1): 0}
+        heapq.heappush(startHeap, (grid[0][0], 0, 0))
+        heapq.heappush(endHeap, (grid[n-1][n-1], n-1, n-1))
         
-      
-        while heap:
-            #print(heap)
-            a = heapq.heappop(heap)
-            h, r, c, visited = a
-            if (r, c) == (n-1, n-1): return h
-            #print(r, c, visited)
-           
-            for newR, newC in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
+        
+        while startHeap and endHeap:
+            sVal, sR, sC = heapq.heappop(startHeap)
+            sVisited[(sR, sC)] = sVal
+            for newSR, newSC in [(sR+1, sC), (sR-1, sC), (sR, sC+1), (sR, sC-1)]:
                 
-                if 0 <= newR < n and 0 <= newC < n and (newR, newC) not in visited:
-                    #visitedCopy = visited.copy()
-                    visited.add((newR, newC))
-                    #print(max(grid[newR][newC], h), newR, newC, visitedCopy)
-                    # why this question does not need a copy of visited?
-                    # because it is greedy?
-                    heapq.heappush(heap, (max(grid[newR][newC], h), newR, newC, visited))
-                  
+                if 0 <= newSR < n and 0 <= newSC < n and (newSR, newSC) not in sVisited:
+
+                    if (newSR, newSC) in eVisited: 
+                        #print(eVisited, "eVisited")
+                        #print(sVisited)
+                        return max(sVal, eVisited[(newSR, newSC)])
+                    heapq.heappush(startHeap, (max(grid[newSR][newSC], sVal), newSR, newSC))
+            
+            eVal, eR, eC = heapq.heappop(endHeap)
+            #print(eVisited)
+            eVisited[(eR, eC)] = eVal
+            for newER, newEC in [(eR+1, eC), (eR-1, eC), (eR, eC+1), (eR, eC-1)]:
+                
+                if 0 <= newER < n and 0 <= newEC < n and (newER, newEC) not in eVisited:
+                    
+                    if (newER, newEC) in sVisited: 
+    
+                        return max(sVisited[(newER, newEC)], eVal)
+                    heapq.heappush(endHeap, (max(grid[newER][newEC], eVal), newER, newEC))
+                    
+                    
+        
+        
+        #Bidirectional BFS
+      
