d1538566c3c7487d95502cac65b13c12f9942ee0
Hang Nguyen <hang.panbee@gmail.com>
2022-04-21 13:53:16 -0400

    Time: 5091 ms (5.01%), Space: 349 MB (5.15%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -4,12 +4,14 @@ class Solution:
         
         q = collections.deque()
         q.append((0, 1, 0))
-        
+        dp2 = {}
         while q:
             pos, speed, count = q.popleft()
-            if pos == target: return count
+         
             reverseSpeed = -1 if speed >= 0 else 1
             for nextPos, nextSpeed, nextCount in [(pos+speed, speed*2, count+1), (pos, reverseSpeed, count+1)]:
                 if (nextPos, nextSpeed) in dp: continue
                 q.append((nextPos, nextSpeed, nextCount))
-                dp[(nextPos, nextSpeed)] = nextCount
+                if nextPos == target: return nextCount
+                dp[(nextPos, nextSpeed)] = (nextCount)
+           
