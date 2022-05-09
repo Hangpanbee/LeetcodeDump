a1f27124a5a3dcc1f7a84f974936867ca219c2fd
Hang Nguyen <hang.panbee@gmail.com>
2022-05-09 13:38:37 -0400

    Time: 3199 ms (30.00%), Space: 77.9 MB (40.00%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
@@ -1,44 +1,30 @@
 class Solution:
     def hasValidPath(self, grid: List[List[str]]) -> bool:
-        #bi directional bfs
-        m, n = len(grid), len(grid[0])
-        sq, eq = [], []
-        START = (0, 0)
-        END = (m-1, n-1)
-        sv, ev = {(0, 0, 0): True}, {(m-1, n-1, 0): True}
-        if (grid[0][0] == ")") or (grid[m-1][n-1] == "("): return False
-        sq.append((START, self.getParaValue(grid[0][0])))
-        eq.append((END, self.getParaValue(grid[m-1][n-1])))
-        
-        while sq and eq:
-            sRC, sParaSum = sq.pop()
-            sR , sC = sRC
-            #Have to define the visited condition and define condition when meets eq
-            for (sDR, sDC) in [(sR+1, sC), (sR, sC+1)]:
-                
-                if (0 <= sDR < m and 0 <= sDC < n and (sDR, sDC, sParaSum + self.getParaValue(grid[sDR][sDC])) not in sv):
-                    currSParaSum = sParaSum + self.getParaValue(grid[sDR][sDC])
-                    if currSParaSum < 0 or currSParaSum >= ((m*n)//2): continue
-                    if (sR, sC, 0 - currSParaSum) in ev: 
-                        #print(0-currSParaSum)
-                        return True
-                    
-                    sq.append(((sDR, sDC), currSParaSum))
-                    sv[(sDR, sDC, currSParaSum)] = True
-            
-            eRC, eParaSum = eq.pop()
-            eR, eC = eRC
-            for (eDR, eDC) in [(eR-1, eC), (eR, eC-1)]:
-                
-                if (0 <= eDR < m and 0 <= eDC < n and (eDR, eDC, eParaSum + self.getParaValue(grid[eDR][eDC])) not in ev):
-                    currEParaSum = eParaSum + self.getParaValue(grid[eDR][eDC])
-                    if currEParaSum > 0 or -currEParaSum >= (m*n)//2 : continue
-                    if (eR, eC, 0 - currEParaSum) in sv: 
-                        #print(sv, eDR, eDC, currEParaSum)
-                        return True
-                    eq.append(((eDR, eDC), currEParaSum))
-                    ev[(eDR, eDC, currEParaSum)] = True
-        
+        n=len(grid)
+        m=len(grid[0])
+        @lru_cache(None)
+        def helper(x,y,count):
+            if x==n-1 and y==m-1:
+                if count==1 and grid[n-1][m-1]==")":
+                    return True
+                else:
+                    return False
+            elif count==-1:
+                return False
+            else:
+                ans=False
+                if grid[x][y]==")":
+                    count-=1
+                else:
+                    count+=1
+                xt=[x+1,x]
+                yt=[y,y+1]
+                for i in range(0,2):
+                    if xt[i]<n and yt[i]<m:
+                        ans=ans or helper(xt[i],yt[i],count)
+                return ans
+        return helper(0,0,0)
+                        
         
     
         return False
