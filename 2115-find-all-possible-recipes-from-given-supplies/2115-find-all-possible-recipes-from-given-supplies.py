305154d08142ebe8896da8c5bc31ffba40a9aa13
Hang Nguyen <hang.panbee@gmail.com>
2022-05-29 09:22:56 -0400

    Time: 1596 ms (24.70%), Space: 17.1 MB (48.18%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        """
+        can the dependencies has duplicate value?
+        what if the target value isnt in range?
+        what if there is a deadend edge?
+        
+        topological sort will handle case where there is a cycle, and case where there is no start
+        read the question more carefully
+        """
+        ingredToRecipes = collections.defaultdict(list)
+        inEdge = {}
+        countRecipes = len(recipes)
+        
+        for i in range(countRecipes):
+            for ii, ingre in enumerate(ingredients[i]):
+                ingredToRecipes[ingre].append(recipes[i])
+                if ingre not in inEdge:
+                    inEdge[ingre] = 0
+            inEdge[recipes[i]] = len(ingredients[i])
+            
+       
+        q = collections.deque()
+        for recipe, lenIngre in inEdge.items():
+            if lenIngre == 0 and recipe in supplies:
+                q.append(recipe)
+         
+        allRecipes = [] 
+        while q:
+            recipe = q.popleft()
+            
+            for nxtRecipe  in ingredToRecipes[recipe]:
+                inEdge[nxtRecipe] -= 1
+                if inEdge[nxtRecipe] == 0:
+                    allRecipes.append(nxtRecipe)
+                    q.append(nxtRecipe)
+                
+        return allRecipes
