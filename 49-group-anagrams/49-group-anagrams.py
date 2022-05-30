5ded826e6fbbd331e402a52aba5d8ddcd19cfa87
Hang Nguyen <hang.panbee@gmail.com>
2022-05-29 21:46:51 -0400

    Time: 98 ms (94.39%), Space: 18.5 MB (41.04%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,27 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        are the words unique
+        """
+        
+        mapLenToStrs = collections.defaultdict(list)
+        for i, string in enumerate(strs):
+            mapLenToStrs[len(string)].append(string)
+        
+        mapHashToGroup = collections.defaultdict(list)
+        
+        for strLen, string in mapLenToStrs.items():
+            for s in string:
+                hashValue = self.calHash(s)
+                mapHashToGroup[(hashValue, strLen)].append(s)
+                #print(hashValue)
+        #print(mapHashToGroup)
+        return mapHashToGroup.values()
+    
+    def calHash(self, string):
+        primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101)
+        hashValue = 1
+        m = 10**9+7
+        for char in string:
+            hashValue *= primes[ord(char) - 97]
+        return hashValue % m
