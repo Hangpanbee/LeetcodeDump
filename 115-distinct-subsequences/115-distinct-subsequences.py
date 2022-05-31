7bdcd80762f9ac24a019ac4c5bc6135e91611e90
Hang Nguyen <hang.panbee@gmail.com>
2022-05-31 10:28:51 -0400

    Time: 1859 ms (5.00%), Space: 257.5 MB (5.31%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -2,6 +2,8 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         memoize = {}
         m, n = len(s), len(t)
+        if m < n: return 0
+        
         @lru_cache(None)
         def dp(i, j):
             if j == n:
