c4f2626dfbf3bd3ab434a9f9e3dfeb128435d5c2
Hang Nguyen <hang.panbee@gmail.com>
2022-06-02 14:32:06 -0400

    Time: 52 ms (26.86%), Space: 14.1 MB (14.95%) - LeetHub

---
+++ 68-text-justification/68-text-justification.py
@@ -0,0 +1,71 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        """
+        for words quesiton, ask if only consist of lowercase and symbol
+        edge case: wher one 1 word fits in a line
+        what if a word is longer than maxWidth????
+        
+        """
+        getWordsLen = lambda wordsInline: sum(len(word) for word in wordsInline)
+        
+        def getWordsPerLine(start):
+            wordsInline, width = [], 0
+            
+            for i in range(start, len(words)):
+                word = words[i]
+                if len(wordsInline) + len(word) + width <= maxWidth:
+                    wordsInline.append(word)
+                    width += len(word)
+                else:
+                    return (i, wordsInline)
+            return (len(words), wordsInline)
+                
+        def evenJustify(wordsInline):
+            sumLen = getWordsLen(wordsInline)
+            spaces = maxWidth - sumLen
+            spaceLots = 1 if len(wordsInline) == 1 else len(wordsInline)-1
+            spacePerLot = spaces//spaceLots
+            spaceArr = [spacePerLot]*spaceLots
+            
+            for extraSpace in range(spaces%spaceLots):
+                spaceArr[extraSpace] += 1
+            
+            justifiedLine = []
+            for s, w in zip(spaceArr, wordsInline):
+                justifiedLine.append(w)
+                justifiedLine.append(s*" ")
+            
+            if len(wordsInline) > 1:
+                justifiedLine.append(wordsInline[-1])
+            
+            return "".join(justifiedLine)
+        
+        def leftJustify(wordsInline):
+            sumLen = getWordsLen(wordsInline)
+            spaces = maxWidth - sumLen
+            justifiedLine = []
+            for word in wordsInline:
+                justifiedLine.append(word)
+                if spaces > 0: justifiedLine.append(" ")
+                spaces -= 1
+            print(spaces)
+            if spaces > 0:
+                justifiedLine.append(" "*spaces)
+            
+            return "".join(justifiedLine)
+        
+        
+        i = 0
+        text = []
+        while i < len(words):
+            nxtI, currWordsInLine = getWordsPerLine(i)
+            text.append(currWordsInLine)
+            i = nxtI
+        
+        
+        for line in range(len(text)-1):
+            text[line] = evenJustify(text[line])
+        
+        text[-1] = leftJustify(text[-1])
+        #print(len(text[-1]))
+        return text
