07902528418f3899ea876ed338c4eb297b8db9ae
Hang Nguyen <hang.panbee@gmail.com>
2022-06-12 09:03:09 -0400

    Time: 154 ms (12.72%), Space: 119 MB (67.54%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,39 +1,25 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-     // positive
-    // subarray that only contains unique -> contiguous
-    // sliding window
-        int l = 0; int r = 0;
-        Map<Integer, Integer> windowCounts = new HashMap<>();
-        int windowSum = 0;
-        int maxScore = 0;
-        while (r < nums.length) {
-            // r = 0, windowCounts = {}
-            // r = 1, windowCounts = {4:1}
-            // r = 2, l = 0, windowCounts = {4:1, 2:1}
-            while (windowCounts.containsKey(nums[r])) {
-                maxScore = Math.max(maxScore, windowSum);
-                // maxScore = 6
-                windowCounts.put(nums[l], windowCounts.get(nums[l])-1);
-                // windowCounts = {2: 1}
-                if (windowCounts.get(nums[l]) == 0) {
-                    windowCounts.remove(nums[l]);
-                }
-                windowSum -= nums[l];
-                // windowSum = 2
-                l++;
-                //l=1
+        int result = 0;
+        int currentSum = 0;
+        HashSet<Integer> set = new HashSet<>();
+        int start = 0;
+        for (int end = 0; end < nums.length; end++) {
+            // increment start until subarray has unique elements
+            while (set.contains(nums[end])) {
+                set.remove(nums[start]);
+                currentSum -= nums[start];
+                start++;
             }
-            
-            //windowCounts = {4: 1, 2: 1}
-            windowCounts.put(nums[r], windowCounts.getOrDefault(nums[r],0)+1);
-            windowSum += nums[r];
-            // windowSum = 4;
-            // windowSum = 6;
-            r++;
+            currentSum += nums[end];
+            set.add(nums[end]);
+            // update result with maximum sum found so far
+            result = Math.max(result, currentSum);
         }
-        
-        return Math.max(maxScore, windowSum);
-        
+        return result;
     }
+        
+        //return Math.max(maxScore, windowSum);
+        
+    
 }
