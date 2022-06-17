0e2272fd3485d0982859ec3e1aa30978013df5fc
Hang Nguyen <hang.panbee@gmail.com>
2022-06-17 16:12:08 -0400

    Time: 4 ms (67.22%), Space: 40.3 MB (92.86%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,60 +1,52 @@
 class Solution {
     public String reorganizeString(String s) {
+        Map<Character, Integer> counter = buildFreqCounter(s);
         
-        Queue<int[]> maxHeap = new PriorityQueue<>(
-            (int[] p1, int[] p2) -> p2[1] - p1[1]
-        );
-           
-        Map<Character, Integer> mapCharToFreq = getCounter(s);    
-            
-        for (Map.Entry<Character, Integer> e: mapCharToFreq.entrySet()) {
-            maxHeap.add(new int[]{e.getKey(), e.getValue()});
+        Queue<int[]> maxHeap = new PriorityQueue<>((int[] i1, int[] i2) -> (i2[0] - i1[0]));
+        int bucket = (s.length()%2==0) ? s.length()/2 : s.length()/2+1;
+        for (Map.Entry<Character, Integer> e: counter.entrySet()) {
+            if (e.getValue() > (bucket) ) return "";
+            maxHeap.add(new int[]{e.getValue(), e.getKey()});
         }
         
-        // aab
-        StringBuilder ans = new StringBuilder();
+        StringBuilder[] organizedChar = new StringBuilder[bucket];
+        for (int i = 0; i < (bucket); i++) {
+            organizedChar[i] = new StringBuilder();
+        }
+        
+        int i = 0;
         while (!maxHeap.isEmpty()) {
-            int[] first = maxHeap.poll();
-            // first [a, 2]
-            // first [a, 1]
-            if (ans.length() == 0 || ans.charAt(ans.length()-1) != (char) first[0]) {
-                ans.append((char) first[0]);
-                // ans = 'a'
-                first[1]--;
-                // first [a, 1]
-                if (first[1] > 0) {
-                    maxHeap.add(first);
-                    // maxHeap = [(a, 1), (b, 1)]
+            int[] curr = maxHeap.poll();
+            // aaabbcc -> a b c a b a c
+            char c = (char) curr[1];
+            while (curr[0] > 0) {
+                if (organizedChar[i].length() == 0 ||  c != organizedChar[i].charAt(organizedChar[i].length()-1) ) {
+                    organizedChar[i].append(c);
                 }
-            } else {
-                if (maxHeap.isEmpty()) return "";
-                int[] scnd = maxHeap.poll();
-                // scnd = ['b', 1]
-                ans.append((char) scnd[0]);
-                // ans = 'ab'
-                scnd[1]--;
-                
-                if (scnd[1] > 0) {
-                    maxHeap.add(scnd);
-                }
-                maxHeap.add(first);
-                
+                i++;
+                i%=organizedChar.length;
+                --curr[0];
             }
             
         }
-
-        if (ans.length() < s.length()) return "";
-        else return ans.toString();
+        //System.out.println(Arrays.toString(organizedChar));
+        
+        for (int j = 1; j < organizedChar.length; j++) {
+            organizedChar[0].append(organizedChar[j]);
+        }
+        
+        return organizedChar[0].toString();
         
     }
     
-    private Map<Character, Integer> getCounter(String s) {
-        Map<Character, Integer> mapCharToFreq = new HashMap<>();
+    private Map<Character, Integer> buildFreqCounter(String s) {
+        Map<Character, Integer> counter = new HashMap<>();
         
         for (char c: s.toCharArray()) {
-            mapCharToFreq.put(c, mapCharToFreq.getOrDefault(c, 0)+1);
+            counter.put(c, counter.getOrDefault(c, 0)+1);
         }
         
-        return mapCharToFreq;
+        return counter;
     }
+    
 }
