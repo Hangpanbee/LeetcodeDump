d30e128bdb17391fea54231edbce5e761b12303d
Hang Nguyen <hang.panbee@gmail.com>
2022-06-30 16:55:39 -0400

    Time: 1229 ms (27.27%), Space: 15.8 MB (33.13%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -9,7 +9,7 @@ class Solution:
         for i, char in enumerate(s):
             ordChar = ord(char) - ord('a')
             
-            charBucketCopy = charBucket[ordChar].copy()
+            charBucketCopy = charBucket[ordChar]
             charBucket[ordChar] = []
             for j, pointer in enumerate(charBucketCopy):
                 #print(pointer, charBucket[ordChar])
