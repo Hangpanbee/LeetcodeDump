88c970151990bcabdd0bbfbcc2c628d05757a94c
Hang Nguyen <hang.panbee@gmail.com>
2022-06-29 22:32:09 -0400

    Time: 56 ms (20.30%), Space: 13.9 MB (73.98%) - LeetHub

---
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        input = input.replace(" ", "_")
+      
+        splittedInput = input.split("\n")
+        #print(len(splittedInput[-1]))
+        # [dir, \tsubdir, \t\tfile1.ext, \t\tsubsubdir1, \tsubdir2, \t\tsubsubdir2, \t\t\tfile2.ext]
+        
+        st = []
+        maxLenPath = 0
+        for i, file in enumerate(splittedInput):
+            fileSplitted = file.split("\t")
+            fileName, fileLevel = fileSplitted[-1], len(fileSplitted)
+            isTextFile = len(file.split(".")) > 1
+            pathLen = len(fileName)
+            while st and st[-1][1] >= fileLevel:
+                st.pop()
+            if st: pathLen += st[-1][2] + 1
+                
+            if isTextFile:    
+                maxLenPath = max(maxLenPath, pathLen)
+            else:
+                st.append((fileName, fileLevel, pathLen))
+        return maxLenPath
+        
