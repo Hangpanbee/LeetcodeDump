d92d4af112640c7f62d55dac138d399ea4ec6227
Hang Nguyen <hang.panbee@gmail.com>
2022-06-30 17:01:54 -0400

    Time: 689 ms (58.09%), Space: 15.7 MB (50.83%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,7 +1,9 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        charBucket = [[] for i in range(26)]
-        
+        charBucket = [collections.deque() for i in range(26)]
+        """
+        run time: worst case: o(m*n) -> m
+        """
         for i, w in enumerate(words):
             charBucket[ord(w[0]) - ord('a')].append((i, 0))
         
@@ -9,17 +11,19 @@ class Solution:
         for i, char in enumerate(s):
             ordChar = ord(char) - ord('a')
             
-            charBucketCopy = charBucket[ordChar]
-            charBucket[ordChar] = []
-            for j, pointer in enumerate(charBucketCopy):
+            
+            lenChar = len(charBucket[ordChar])
+            i = 0
+            while i < lenChar:
                 #print(pointer, charBucket[ordChar])
-                w, currP = pointer
+                w, currP = charBucket[ordChar].popleft()
                 word = words[w]
                 if (currP + 1) == len(word): 
                     ans += 1
                 else:
                     nxtOrd = ord(word[currP+1]) - ord('a')
                     charBucket[nxtOrd].append((w, currP+1))
+                i+=1
             
         return ans
         
