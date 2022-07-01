0a1a86528c96a8389b1694cf08103c955e472fbe
Hang Nguyen <hang.panbee@gmail.com>
2022-06-30 22:01:56 -0400

    Time: 71 ms (23.95%), Space: 14.6 MB (29.46%) - LeetHub

---
+++ 900-rle-iterator/900-rle-iterator.py
@@ -0,0 +1,39 @@
+class RLEIterator:
+
+    def __init__(self, encoding: List[int]):
+        """
+        [(3,8),(5,5),(6,8)]
+        """
+        runningSum = 0
+        self.encoding = []
+        for i in range(0, len(encoding), 2):
+            if encoding[i] == 0: continue
+            runningSum += encoding[i]
+            self.encoding.append((runningSum, encoding[i+1]))
+        self.iterator = 0
+
+    def next(self, n: int) -> int:
+        self.iterator += n
+        # self.i = 4
+        targetI = self.__binarySearch(self.iterator)
+        if targetI >= len(self.encoding):
+            return -1
+        else: return self.encoding[targetI][1]
+    
+    def __binarySearch(self, target) -> int:
+        l, r = 0, len(self.encoding)
+        
+        while l < r:
+            mid = l + (r-l)//2
+            
+            if self.encoding[mid][0] < target:
+                l = mid + 1
+            else:
+                r = mid
+                
+        return l
+
+
+# Your RLEIterator object will be instantiated and called as such:
+# obj = RLEIterator(encoding)
+# param_1 = obj.next(n)
