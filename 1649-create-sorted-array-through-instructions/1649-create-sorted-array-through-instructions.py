defb1a1c8b2ddef283760d644042e0176d6653a7
Hang Nguyen <hang.panbee@gmail.com>
2022-07-02 22:27:47 -0400

    Time: 6278 ms (74.13%), Space: 29.9 MB (40.56%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -1,12 +1,31 @@
 from sortedcontainers import SortedList
 
 class Solution:
-    def createSortedArray(self, instructions):
-        SList = SortedList()
-        ans = 0
-        for num in instructions:
-            ans += min(SList.bisect_left(num), len(SList) - SList.bisect_right(num))
-            ans %= (10**9 + 7)
-            SList.add(num)
-
-        return ans
+    def createSortedArray(self, instructions: List[int]) -> int:
+        """
+            [ 1, 5, 6, 2]
+            [1]
+            [1,5]
+            [1,5,6]
+            [1,2,5,6]
+            think about duplicate
+        """
+        arr = SortedList()
+        m = 10**9 + 7
+        sumCost = 0
+        for i, instruction in enumerate(instructions):
+            s, l = self.update(arr, instruction)
+            sumCost += min(s, l) % m
+        
+  
+        return sumCost % m
+        
+        
+        
+    def update(self, arr, target):
+        si = arr.bisect_left(target)
+        arr.add(target)
+        li = len(arr) - arr.bisect_right(target) 
+        
+        
+        return (si, li)
