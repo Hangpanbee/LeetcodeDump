c67c6e4d762b534e376f3505d6ab6273bdac6b24
Hang Nguyen <hang.panbee@gmail.com>
2022-07-02 21:12:27 -0400

    Time: 3852 ms (58.34%), Space: 32.5 MB (80.40%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,53 +1,19 @@
-
-class Solution(object):
-    def countSmaller(self, nums):
-        for i, v in enumerate(nums):
-            nums[i] = [v, i]
-        ans = [0] * len(nums)
-        
-    
-        def merge(left, right):
-            l, r = 0, 0
-            out = []
-            prefix_sum = 0
-            #print(left, right)
-            while l < len(left) and r < len(right):
-                if left[l][0] <= right[r][0]:
-                    out.append(left[l])
-                    ans[left[l][1]] += prefix_sum
-                    l+=1
-                elif left[l][0] > right[r][0]:
-                    out.append(right[r])
-                    prefix_sum += 1
-                    r+=1
-
-            if r < len(right):
-                out.extend(right[r:])
-                #out += right[r:]
-                
-            while l < len(left):
-                ans[left[l][1]] += prefix_sum
-                out.append(left[l])
-                l += 1
-
-            #print(out)
-            return out
-
-
-        def mergeSort(nums):
-            #the partition method
-            if len(nums) == 1:
-                return nums
-
-            mid = len(nums)//2
-
-            left = mergeSort(nums[:mid])
-            right = mergeSort(nums[mid:])
-
-            return merge(left, right)
-
-        mergeSort(nums)
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        """
+        binary search way
+        """
+        ans = [-1]*len(nums)
+        newNums = []
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] = self.update(newNums, nums[i])
+            
         return ans
         
         
         
+    def update(self, nums, target):
+        nI = bisect.bisect_left(nums, target)
+        nums.insert(nI, target)
+        
+        return nI
