f72dfc92e6b422774aa78b411888183ff2e767aa
Hang Nguyen <hang.panbee@gmail.com>
2022-07-04 22:54:49 -0400

    Time: 165 ms (93.91%), Space: 18.9 MB (74.49%) - LeetHub

---
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -0,0 +1,43 @@
+class SummaryRanges:
+
+    def __init__(self):
+        self.intervals = []        
+
+    def addNum(self, val: int) -> None:
+        vali = bisect.bisect_right(self.intervals, val)
+
+        if vali%2 == 1: pass
+        else:
+            #if val == 60: 
+                #print(self.intervals[26], val, vali)
+            if vali+1 < len(self.intervals) and vali-1 >= 0 and self.intervals[vali] == val+1 and self.intervals[vali-1] == val-1:
+                #print(self.intervals, val)
+                self.intervals[vali-2:vali+2] = [self.intervals[vali-2], self.intervals[vali+1]]
+            elif vali+1 < len(self.intervals) and self.intervals[vali] == val+1:
+                self.intervals[vali] = val
+            elif vali-1 >= 0 and self.intervals[vali-1] == val-1:
+                self.intervals[vali-1] = val
+            elif vali+1 < len(self.intervals) and self.intervals[vali] == val:
+                pass
+            elif vali-1 >= 0 and self.intervals[vali-1] == val:
+                pass
+            else:
+                bisect.insort_left(self.intervals, val)
+                bisect.insort_right(self.intervals, val)
+        
+
+    def getIntervals(self) -> List[List[int]]:
+        intervals = []
+        #print(self.intervals)
+        for i in range(0, len(self.intervals),2):
+            #print(i)
+            #print(self.intervals[i+1])
+            intervals.append([self.intervals[i], self.intervals[i+1]]) 
+        return intervals
+        
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(val)
+# param_2 = obj.getIntervals()
