9c0580a50ebfba0a4c29c64fad266728485a2e50
Hang Nguyen <hang.panbee@gmail.com>
2022-07-06 12:41:32 -0400

    Time: 734 ms (28.52%), Space: 16.4 MB (85.68%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -1,27 +1,9 @@
+
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
-        """
-            1   1   0
-            0   0   1
-            1   1   0
-            
-            1   1   0       1   1   0
-            0   1   1       1   0   0 
-            1   1   0       0   0   1
-        """
-        pattern1 = tuple(grid[0])
-        pattern2 = []
-        for i, v in enumerate(grid[0]):
-            pattern2.append(1-v)
-            
-        pattern2 = tuple(pattern2)
-        m, n = len(grid), len(grid[0])
-        
-        for r in range(m):
-            currR = tuple(grid[r])
-            
-            if currR != pattern1 and currR != pattern2:
+        match = grid[0]
+        match_flip = [1 if x==0 else 0 for x in grid[0]]
+        for i in range(1, len(grid)):
+            if grid[i] != match and grid[i] != match_flip:
                 return False
-        
         return True
-        
